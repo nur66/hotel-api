3bbce726cb04f6537440ac50ad14b98c37dc4f0e
unknown <nuriswanto66@gmail.com>
2022-11-26 08:36:27 -0800

    crud hotels, rooms, users

--- controllers/room.js
+++ controllers/room.js
@@ -33,12 +33,20 @@ export const updateRoom = async (req, res, next) => {
 };
 
 export const deleteRoom = async (req, res, next) => {
-  try {
-    const deleteRoom = await Room.findByIdAndDelete(req.params.id);
-    res.status(200).json("Room has been deleted");
-  } catch (err) {
-    next(err);
-  }
+    const hotelId = req.params.hotelid;
+    try {
+        const deleteRoom = await Room.findByIdAndDelete(req.params.id);
+        try {
+            await Hotel.findByIdAndUpdate(hotelId, {
+                $pull: {rooms: req.params.id}
+            });
+        } catch (error) {
+            next(error);
+        }
+        res.status(200).json("Room has been deleted");
+    } catch (err) {
+        next(err);
+    }
 };
 
 export const getRoom = async (req, res, next) => {
