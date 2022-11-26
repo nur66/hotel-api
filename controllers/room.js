040b408d3794375e5449616c3ed2032f055a231b
unknown <nuriswanto66@gmail.com>
2022-11-26 01:52:01 -0800

    room

---
+++ controllers/room.js
@@ -0,0 +1,22 @@
+import Room from '../models/Room.js';
+import Hotel from '../models/Hotel.js';
+import { createError } from '../utils/errors.js';
+
+export const createRoom = async (req, res, next) => {
+    const hotelId = req.body.hotelid;
+    const newRoom = new Room(req.body);
+
+    try {
+        const savedRoom = await newRoom.save();
+        try {
+            await Hotel.findByIdAndUpdate(hotelId, {
+                $push: { rooms: savedRoom._id }
+            })
+        } catch (err) {
+            next(err);
+        }
+        res.status(200).json(savedRoom);
+    } catch (err) {
+        
+    }
+}
