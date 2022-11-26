17da098ba05cdb8226a18c760d41c25998eadf4c
unknown <nuriswanto66@gmail.com>
2022-11-26 04:12:21 -0800

    create rooms

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,9 +1,22 @@
 import express from "express";
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send("hello this is auth endpoint")
-})
+// CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+// UPDATE
+router.put("/:id", verifyAdmin, updateRoom);
+
+// DELETE
+router.delete("/:id", verifyAdmin, deleteRoom);
+
+// GET
+router.get("/:id", getRoom);
+
+// GET ALL
+router.get("/", getRooms);
 
 export default router;
